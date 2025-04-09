42badb80860e39858def8cd2a60c1ea3b67eed1c
wjdtpdus9343 <jungsaeyun@gmail.com>
2025-04-09 09:53:06 +0900

    MusicStreamingService 중간과정까지

---
+++ Lec04_Array_Class/MusicStreaming/MusicStreaming/main.cpp
@@ -0,0 +1,14 @@
+//#include "Complex.h"
+//#include "SportCar.h"
+
+#include "Music.h"
+
+
+int main() {
+
+	// music streaming service¸¦ »ý¼º
+	MusicStreamingService my_service("spotify");
+	my_service.addMusic("PolaroidLove", "ENHYPEN","DIMENSION", 2022);
+	my_service.addMusic("Ditto", "NewJeans", "ALBUM", 2023);
+
+}
